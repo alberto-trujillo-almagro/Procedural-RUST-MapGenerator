10ab829061015e194f84934d255fe3e4f92a628c
alberto-trujillo-almagro <10946358+alberto-trujillo-almagro@users.noreply.github.com>
2024-01-20 18:14:43 +0100

    Update generator.bat

--- generator.bat
+++ generator.bat
@@ -16,7 +16,7 @@ for /f "tokens=1-8 delims=.:/ " %%a in ("%date% %time%") do set DateNtime=%%a-%%
 SET CURRENTDIR=%cd%
 SET LOGFILE=%CURRENTDIR%\LogFile_%DateNtime%.log
 SET LOGFILE2=%CURRENTDIR%\LogFile2_%DateNtime%.log
-@rmdir /S /Q "%CURRENTDIR%\server" >nul
+REM -------- @rmdir /S /Q "%CURRENTDIR%\server" >nul
 
 IF NOT exist "%CURRENTDIR%\MAPS" ( @mkdir "%CURRENTDIR%\MAPS" >nul )
 IF NOT exist "%CURRENTDIR%\server" ( @mkdir "%CURRENTDIR%\server" >nul )
@@ -29,27 +29,46 @@ echo.
 echo.
 echo.
 echo                                   ********************************************************
-echo                                   *         RUST PROCEDURAL MAP GENERATOR v1.0           *
+echo                                   *         RUST PROCEDURAL MAP GENERATOR v1.1           *
 echo                                   ********************************************************
 echo                                   *   1. Select Map Size                                 *
-echo                                   *   2. Generate Map!                                   *
-echo                                   *   3. View Map Prefabs and config                     *
-echo                                   *   4. View Maps generated                             *
-echo                                   *   5. Exit                                            *
+echo                                   *   2. Select Map Seed                                 *
+echo                                   *   3. Generate Map!                                   *
+echo                                   *   4. View Map Prefabs and config                     *
+echo                                   *   5. View Maps generated                             *
+echo                                   *   6. Exit                                            *
 echo                                   ********************************************************
 echo.                                           [MapSize: %MAPSIZE% - WorldSeed: %RANDOMSEED%]
 echo.
-CHOICE /C 12345 /N /M "Select Option (1,2,3,4,5):"
+CHOICE /C 123456 /N /M "Select Option (1,2,3,4,5,6):"
 echo.
-IF ERRORLEVEL ==5 GOTO END
-IF ERRORLEVEL ==4 GOTO VIEWMAPS
-IF ERRORLEVEL ==3 GOTO VIEWCONFIG
-IF ERRORLEVEL ==2 GOTO GENERATEMAP
+IF ERRORLEVEL ==6 GOTO END
+IF ERRORLEVEL ==5 GOTO VIEWMAPS
+IF ERRORLEVEL ==4 GOTO VIEWCONFIG
+IF ERRORLEVEL ==3 GOTO GENERATEMAP
+IF ERRORLEVEL ==2 GOTO MAPSEED
 IF ERRORLEVEL ==1 GOTO MAPSIZE
 goto END
 
+:MAPSEED
+set /P RANDOMSEED=Input MapSeed [0-2147483647]:
+IF %RANDOMSEED% GTR 2147483647 (
+echo MAPSEED must be between 0 and 2147483647.
+goto MAPSEED
+)
+cls
+goto MENU
+
 :MAPSIZE
 set /P MAPSIZE=Input MapSize [1500-6000]:
+IF %MAPSIZE% LSS 1000 (
+echo MAPSIZE must be between 1000 and 6000.
+goto MAPSIZE
+)
+IF %MAPSIZE% GTR 6000 (
+echo MAPSIZE must be between 1000 and 6000.
+goto MAPSIZE
+)
 cls
 goto MENU
 
@@ -68,14 +87,19 @@ echo [INFO][%DATE% - %TIME%] RCON IS ALIVE >> %LOGFILE%
 IF EXIST server\world_%RANDOMSEED%\*.map (
     dir server\world_%RANDOMSEED%\*.map
     copy /Y server\world_%RANDOMSEED%\*.map "%CURRENTDIR%\MAPS\"
+    "%CURRENTDIR%\rcon.exe" -a 127.0.0.1:28016 -t web -p 1 world.rendermap 2>nul
+    IF EXIST map_%MAPSIZE%_%RANDOMSEED%.png (
+       copy /Y map_%MAPSIZE%_%RANDOMSEED%.png "%CURRENTDIR%\MAPS\"
+       rename "%CURRENTDIR%\MAPS\map_%MAPSIZE%_%RANDOMSEED%.png" proceduralmap.%MAPSIZE%.%RANDOMSEED%.png
+    )
     timeout /t 20 /nobreak >nul
 )
 "%CURRENTDIR%\rcon.exe" -a 127.0.0.1:28016 -t web -p 1 quit
 timeout /t 30 /nobreak >nul
 cd "%CURRENTDIR%"
 rmdir /S /Q "%CURRENTDIR%\server"
-del "%CURRENTDIR%\%LOGFILE%"
-del "%CURRENTDIR%\%LOGFILE2%"
+del "%LOGFILE%"
+del "%LOGFILE2%"
 goto MENU
 ) ELSE (
 echo [INFO][%DATE% - %TIME%] RCON IS NOT ALIVE >> %LOGFILE%
